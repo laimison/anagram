277e46d8201c7ed2438b11fff928cc7274450178
Laimis Valatka <laimis.valatka@renthopper.co.uk>
2018-10-17 00:42:33 +0100

    added main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   scope module: 'api' do
     namespace :v1 do
       resources :anagrams, only: [:index, :show]
     end
   end
+
+  get '*other', to: 'static#index'
 end
